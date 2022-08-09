36d81b9acff3834be1de956f830cda2a9b5209dc
MamtaLonbale <mamtalonbale12@gmail.com>
2022-08-09 14:20:36 +0530

    first commit

---
+++ src/main/java/log4jEx.java
@@ -0,0 +1,10 @@
+import java.util.logging.Logger;
+
+public class log4jEx {
+	static Logger log=Logger.getLogger(log4jEx.class.getName());
+	public static void main(String[] args) {
+		log.info("this is an info massage");
+
+	}
+
+}
